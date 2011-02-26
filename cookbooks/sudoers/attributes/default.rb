afcf9c1eac8841a90d93ccde4b135932a05ad53d
Anthony Williams <hi@antw.me>
2011-02-26 23:48:17 +0000

    Add sudoers cookbook.

---
+++ cookbooks/sudoers/attributes/default.rb
@@ -0,0 +1,3 @@
+# Users who should be added to the sudoers file.
+default[:auth][:sudo][:users]  = []
+default[:auth][:sudo][:groups] = []
---
+++ cookbooks/sudoers/recipes/default.rb
@@ -0,0 +1,16 @@
+# A recipe which adds the given usernames and groups to the sudoers
+# file.
+
+# Start by updating the sudo package.
+package('sudo') { action(:upgrade) }
+
+# Process the sudoers template.
+template '/etc/sudoers' do
+  source 'sudoers.erb'
+  mode    0440
+  owner  'root'
+  group  'root'
+
+  variables :users  => node[:auth][:sudo][:users],
+            :groups => node[:auth][:sudo][:groups]
+end
