63db0a67dc9d863d1d3d12a6ff778df4619541d2
Anthony Williams <hi@antw.me>
2011-02-27 15:16:13 +0000

    Restart sshd after copying config.

--- cookbooks/openssh/recipes/default.rb
+++ cookbooks/openssh/recipes/default.rb
@@ -4,6 +4,13 @@
 # Update OpenSSH.
 package('openssh') { action(:upgrade) }
 
+# The sshd service.
+service 'sshd' do
+  service_name    'sshd'
+  restart_command '/etc/init.d/sshd restart'
+  supports        [ :restart ]
+endq
+
 # Process the sshd_config template.
 template '/etc/ssh/sshd_config' do
   source 'sshd_config.erb'
@@ -15,3 +22,5 @@ template '/etc/ssh/sshd_config' do
             :disable_root  => node[:openssh][:disable_root],
             :password_auth => node[:openssh][:password_auth]
 end
+
+service('sshd') { action(:restart) }
